e7ec9aa8d3e6a460528634e728c1a6e708121d76
urmit sardhara <sardharaurmit@gmail.com>
2026-04-22 10:44:02 +0530

    add new callback fun tech

--- call_back_async/app.js
+++ call_back_async/app.js
@@ -1,3 +1,13 @@
+// async--------------------------
+
+
+
+
+
+
+
+
+
 // function hello (){
 //     console.log("hello asyn");
 // }
@@ -9,11 +19,54 @@
 
 
 
-console.log("one");
-console.log("two");
+// console.log("one");
+// console.log("two");
 
-setTimeout(() => {
-    console.log("end");
-}, 2000);
+// setTimeout(() => {
+//     console.log("end");
+// }, 2000);
 
-console.log("three");
+// console.log("three");
+
+
+
+
+
+// callback --------------
+ 
+ 
+ 
+ 
+
+// function sum (a,b){
+
+//     // console.log(a+b);
+
+// }
+// function callback (a,b,sumcall){
+//     sumcall(a,b);
+
+// }
+// // callback(1,2,sum); 
+// callback(1,2,(a,b)=>{
+//     console.log(a+b); 
+// })
+
+
+// call data step by step by useing nested call
+function database(data,nextdata){
+    
+    setTimeout(() => {
+        console.log("the data is ",data);
+        if(nextdata){
+            nextdata();
+        }
+
+    }, 2000);
+}
+
+database(1,()=>{
+    database(2,()=>{
+    database(3)
+})
+});
