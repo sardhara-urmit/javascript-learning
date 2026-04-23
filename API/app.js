985308c48a01879cf0a425d2fa4df07a26a6b406
urmit sardhara <sardharaurmit@gmail.com>
2026-04-23 12:08:19 +0530

    use api

---
+++ API/app.js
@@ -0,0 +1,46 @@
+let url ="https://cataas.com/cat";
+
+// let prms =fetch(url);
+// console.log(prms);
+
+
+
+
+// using normal functiopn ----------------------------- 
+
+
+// async function getf (){
+    
+//     console.log("data is comming .....")
+//     let respose =await fetch(url);
+//     console.log(respose);
+    
+// }
+// let s =getf();
+
+
+// thi  is arrow function -------------------------------
+
+
+const getdata = async ()=>{
+    console.log("data is comming .....")
+    let prms =await fetch(url);
+    console.log(prms);
+//    let data= await prms.json();         // this is use for text   .json()
+let data =await prms.blob();               // this is for img        .blob()
+   console.log(data);
+
+}
+
+let p=getdata();
+
+
+//// tis is used to call function step dy step ------------------
+
+
+// async function info(){
+//     await getdata();
+//     await getf();
+// }
+
+// let t =info();
